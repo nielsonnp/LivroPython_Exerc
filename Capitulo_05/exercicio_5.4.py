4855d52d7d74ce9a0a1ae41538c66f2637179b8c
Ranyelson Dantas <nielsonnp@gmail.com>
2018-10-23 10:53:53 -0300

    While - Imprimindo números

---
+++ Capitulo_05/exercicio_5.4.py
@@ -0,0 +1,17 @@
+#Imprimindo número de 0 até o numero digitado pelo usuário
+# fim = int(input('Digite o último número a imprimir: '))
+#
+# x = 0
+#
+# while x <= fim:
+#     print(x)
+#     x = x + 1
+
+#Imprimindo número de 1 até o numero digitado pelo usuário. Apenas números ímpares.
+
+fim = int(input('Digite último número a imprimir: '))
+
+x = 1
+while x <= fim:
+    print(x)
+    x = x + 2
