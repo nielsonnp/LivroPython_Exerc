28a06277a823a610200bbb6ce31d1226204317b0
Ranyelson Dantas <nielsonnp@gmail.com>
2018-10-23 10:58:39 -0300

    While - Tabuada de números.

---
+++ Capitulo_05/exercicio_5.6.py
@@ -0,0 +1,26 @@
+'''Programa no formato de tabuada determinando o inicio da tabuada de 1 e o fim até 10
+Pedindo ao usuário que digite o número da tabuada'''
+
+# n = int(input('Tabuada de: '))
+# x = 1
+#
+# while x <= 10:
+#     print('{} x {} = {}'.format(n, x, n*x))
+#     x=x+1
+
+'''Programa que pedi pra o usuario digitar o inicio e o fim da tabuada
+o fim é onde vai multiplicar'''
+
+x = int(input('Digite o ìnicio: '))
+n = int(input('Digite o fim: '))
+
+while x <= n:
+    print('{} x {} = {}'.format(n, x, n*x))
+    x=x+1
+
+
+
+
+
+
+
